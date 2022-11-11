003ccc79079d7d61510d3c38990ff1e2ff530ce0
Юрий Пеupside <upside89@gmail.com>
2022-11-11 16:27:49 +0500

    init

---
+++ src/CycleOrmBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Upside\CycleOrmBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CycleOrmBundle extends Bundle
+{
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+
+}

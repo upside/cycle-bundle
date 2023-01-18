d5d515e22b635e6cc08179bddf998593e2902345
Юрий Пеupside <upside89@gmail.com>
2023-01-18 05:52:56 +0500

    v6.2

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -4,6 +4,7 @@ use Cycle\Database\Config\DatabaseConfig;
 use Cycle\Database\DatabaseManager;
 use Cycle\ORM\EntityManager;
 use Cycle\ORM\ORM;
+use Cycle\ORM\ORMInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Upside\CycleOrmBundle\DatabaseConfigFactory;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\abstract_arg;
@@ -16,7 +17,7 @@ return static function (ContainerConfigurator $container) {
             ->factory([DatabaseConfigFactory::class, 'create'])
         ->set(DatabaseManager::class)
             ->autowire()
-        ->set(ORM::class)
+        ->set(ORMInterface::class, ORM::class)
             ->arg('$schema', abstract_arg('Cycle orm schema'))
             ->autowire()
         ->set(EntityManager::class)
